4b25a3a64c13fd621bf9a41b1f5f7990ad193369
cjffcnx <krishnanepal725@gmail.com>
2025-10-09 09:15:23 +0545

    User model and schema

---
+++ model/userModel.js
@@ -0,0 +1,41 @@
+const mongoose=require('mongoose');
+
+const userSchema=new mongoose.Schema({
+
+    role:{
+       type:String,
+       require:[true,'Role is required'],
+       enum:['admin','organization','user','hospital']
+
+    },
+    name:{
+type:String,
+required:function(){
+    if(this.role=='user'|| this.role=='admin'){
+        return true;
+    }
+    return false
+}
+    },
+    email:{
+        type:String,
+        require:[true,'Email is required'],
+        unique:true
+    },
+    password:{
+        type:String,
+        require:[true, "Password is required"]
+    },
+    website:{
+        type:String,
+        
+    },
+    address:{
+        type:String,
+        require:[true, "Address is required"]
+    },
+    phone:{
+        type:String,
+        require:[true, "Phone number is required"]
+    }
+})
