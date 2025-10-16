1336ff4efea211b1709de3b34c2d5ff2610f896e
cjffcnx <krishnanepal725@gmail.com>
2025-10-16 18:53:54 +0545

    Added user login and logout functionality now start 5:00:16

--- client/src/components/shared/Layout/Header.js
+++ client/src/components/shared/Layout/Header.js
@@ -2,8 +2,19 @@ import React from 'react'
 import { BiSolidDonateBlood } from "react-icons/bi";
 import { FaUserCircle } from "react-icons/fa";
 import {useSelector} from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 const Header = () => {
+const navigate=useNavigate()
+
 const {user} = useSelector((state)=>state.auth)
+//Logout handler
+const handleLogout=()=>{
+    localStorage.clear();
+    toast.success('Logout Successfully')
+    navigate('/login');
+
+}
 
   return (
 <>
@@ -12,7 +23,7 @@ const {user} = useSelector((state)=>state.auth)
             <div className='navbar-brand'><BiSolidDonateBlood color='red' />Blood Axis</div>
         <ul className='navbar-nav flex-row'>
           <li className='nav-item mx-3'>
-            <p className='nav-link'><FaUserCircle color='green' />WELCOME {user }</p>
+            <p className='nav-link'><FaUserCircle color='green' />WELCOME</p>
           </li>
            <li className='nav-item mx-3'> 
        <button
@@ -23,7 +34,7 @@ const {user} = useSelector((state)=>state.auth)
     margin: '0 auto'
   }}
   className='btn btn-danger'
->
+ onClick={handleLogout}>
   Logout
 </button>
           </li>
