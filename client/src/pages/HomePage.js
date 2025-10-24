3f30aab4433757a63cdefe6d43e92b4a80a37e7b
cjffcnx <krishnanepal725@gmail.com>
2025-10-24 18:14:52 +0545

    Successfully display the in inventory record

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { useSelector } from 'react-redux'
 import Spinner from '../components/shared/Spinner'
 import { toast } from 'react-toastify'
 import Layout from '../components/shared/Layout/Layout'
 import Modal from '../components/shared/modal/Modal'
+import API from '../services/API'
+import moment from 'moment'
 
 const HomePage = () => {
 
   const { loading, error } = useSelector((state) => state.auth)
+  const [data, setData] =useState([]);
+
+
+  //get inventory data
+  const getBloodRecords = async () => {
+try {
+  const {data}= await API.get('/inventory/get-inventory');
+
+  if(data?.success){
+    setData(data?.inventory);
+    console.log("Inventory data:", data);
+  }
+} catch (error) {
+  console.log(error);
+}
+  }
+  
+  useEffect(() => {
+    getBloodRecords();
+  }, []);
   return (
 
 <Layout>
@@ -23,6 +45,30 @@ const HomePage = () => {
   
   Add Inventory 
   </h4>
+
+  <table className="table container">
+  <thead>
+    <tr>
+      <th scope="col">Blood Group</th>
+      <th scope="col">Inventory Type</th>
+      <th scope="col">Quantity</th>
+      <th scope="col">Donar Email</th>
+       <th scope="col">Time and Date</th>
+    </tr>
+  </thead>
+  <tbody>
+  {data && data.map((record)=>(
+    <tr key={record._id}>
+      <td>{record.bloodGroup}</td>
+      <td>{record.inventoryType}</td>
+      <td>{record.quantity}</td>
+      <td>{record.donarEmail}</td>
+      <td>{moment(record.createdAt).format('DD/MM/YYYY, hh:mm a')}</td>
+    </tr>
+  ))}
+  </tbody>
+</table>
+
   <Modal/>
   </>
 )}
